ab25b392ec0d96a12c38c359c201226945b75590
Kunita-B <kunita.boonyong@gmail.com>
2024-07-01 12:00:39 +0700

    init project

---
+++ src/Application/WeatherForecast/Queries/WeatherForecastQuery.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+// using MediatR;
+
+namespace Application.WeatherForecast.Queries
+{
+    public class WeatherForecastQuery //: IRequest<WeatherForecastDto>
+    {
+
+    }
+}
